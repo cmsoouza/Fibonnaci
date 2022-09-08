edaba77b6969c380529714afd412816315bfe6a1
Cristian Souza <cmsoouzaac@outlook.com>
2022-09-08 18:41:31 -0300

    sequencia de fibanacci

---
+++ fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class MainClass
+{
+    public static void Main()
+    {
+        int numeroAnterior = 0;
+        int numeroActual = 1;
+        int novoNumero;
+        int fibonacci;
+
+        for (int i = 0; i < 100; i++)
+        {
+            fibonacci = numeroAnterior + numeroActual;
+            Console.WriteLine(fibonacci);
+            numeroAnterior = numeroActual;
+            numeroActual = fibonacci;
+        }
+    }
+}
+
